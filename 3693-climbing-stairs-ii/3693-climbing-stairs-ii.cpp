fb53fbb435109ef97b257dcb987ff28b7cb96678
omkarjagtap1011 <126784997+omkarjagtap1011@users.noreply.github.com>
2026-05-23 21:13:41 +0530

    Time: 1 ms (97.02%), Space: 177.8 MB (59.47%) - LeetHub

--- 3693-climbing-stairs-ii/3693-climbing-stairs-ii.cpp
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.cpp
@@ -1,19 +1,17 @@
 class Solution {
-    int recur(vector<int>& costs, int i, vector<int>& dp){
-        if(i==0) return 0;
-
-        if(dp[i] != -1) return dp[i];
-
-        int c = costs[i-1];
-        int res = recur(costs, i-1, dp) + c + 1;
-        if(i>1) res = min(res, recur(costs, i-2, dp) + (costs[i-1] + 4));
-        if(i>2) res = min(res, recur(costs, i-3, dp) + (costs[i-1] + 9));
-
-        return dp[i] = res;
-    }
 public:
     int climbStairs(int n, vector<int>& costs) {
         vector<int> dp(n+1, -1);
-        return recur(costs, n, dp);
+        dp[0] = 0;
+
+        for(int i=1; i<=n; i++){
+            int c = costs[i-1];
+            int res = dp[i-1] + c + 1;
+            if(i>1) res = min(res, dp[i-2] + (costs[i-1] + 4));
+            if(i>2) res = min(res, dp[i-3] + (costs[i-1] + 9));
+
+            dp[i] = res;
+        }
+        return dp[n];
     }
 };
