57063ebba016b74186fe242a96579b2334453981
omkarjagtap1011 <126784997+omkarjagtap1011@users.noreply.github.com>
2026-05-24 00:03:17 +0530

    Time: 0 ms (100%), Space: 7.8 MB (57.65%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,17 +1,6 @@
 class Solution {
-    bool whoWins(int n, bool isAliceTurn, vector<int> dp){
-        if(n==1) return !(isAliceTurn);
-        if(dp[n] != -1) return dp[n];
-        for(int i=1; i<n; i++){
-            if(n%i == 0){
-                return dp[n] = whoWins(n-i, !isAliceTurn, dp);
-            }
-        }
-        return dp[n] = !(isAliceTurn);
-    }
 public:
     bool divisorGame(int n) {
-        vector<int> dp(n+1, -1);
-        return whoWins(n, true, dp);
+        return !(n&1);
     }
 };
