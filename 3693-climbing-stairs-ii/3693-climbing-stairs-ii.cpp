7985896563bac6ba029aafc2c628a3549f70b4f5
omkarjagtap1011 <126784997+omkarjagtap1011@users.noreply.github.com>
2026-05-23 21:17:05 +0530

    Time: 0 ms (100%), Space: 169.7 MB (89.89%) - LeetHub

--- 3693-climbing-stairs-ii/3693-climbing-stairs-ii.cpp
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int climbStairs(int n, vector<int>& costs) {
-        vector<int> dp(n+1, -1);
-        dp[0] = 0;
+        int prev1 = 0;
+        int prev2 = 0;
+        int prev3 = 0;
 
         for(int i=1; i<=n; i++){
             int c = costs[i-1];
-            int res = dp[i-1] + c + 1;
-            if(i>1) res = min(res, dp[i-2] + (costs[i-1] + 4));
-            if(i>2) res = min(res, dp[i-3] + (costs[i-1] + 9));
+            int res = prev1 + c + 1;
+            if(i>1) res = min(res, prev2 + (costs[i-1] + 4));
+            if(i>2) res = min(res, prev3 + (costs[i-1] + 9));
 
-            dp[i] = res;
+            prev3 = prev2;
+            prev2 = prev1;
+            prev1 = res;
         }
-        return dp[n];
+        return prev1;
     }
 };
