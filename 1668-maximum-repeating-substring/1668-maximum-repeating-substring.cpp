5fc4b5ec20067505c6e907bedb2939f9bd5eb533
omkarjagtap1011 <126784997+omkarjagtap1011@users.noreply.github.com>
2026-05-24 00:34:40 +0530

    Time: 1 ms (15.37%), Space: 9.3 MB (15.37%) - LeetHub

--- 1668-maximum-repeating-substring/1668-maximum-repeating-substring.cpp
+++ 1668-maximum-repeating-substring/1668-maximum-repeating-substring.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     int maxRepeating(string sequence, string word) {
-        string repeat = word;
-        int count = 0;
+        int n = sequence.size();
+        int m = word.size();
 
-        while(sequence.contains(repeat)){
-            count++;
-            repeat += word;
+        vector<int> dp(n+1, 0);
+        int ans = 0;
+
+        for(int i=m; i<=n; i++){
+            if(sequence.substr(i-m, m) == word){
+                dp[i] = dp[i - m] + 1;
+                ans = max(ans, dp[i]);
+            }
         }
-        return count;
+        return ans;
     }
 };
