97edd5ad81320bdba3f3c5e97ada38884979b55f
omkarjagtap1011 <126784997+omkarjagtap1011@users.noreply.github.com>
2026-05-23 16:32:37 +0530

    Time: 0 ms (100%), Space: 8.2 MB (17.7%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,12 @@
 class Solution {
+    int calculateFib(int n, vector<int>& dp){
+        if(n<=1) return n;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = calculateFib(n-1, dp) + calculateFib(n-2, dp);
+    }
 public:
     int fib(int n) {
-        if(n<=1) return n;
-        return fib(n-1) + fib(n-2);
+        vector<int> dp(n+1, -1);
+        return calculateFib(n, dp);
     }
 };
