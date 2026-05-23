d003aa275fa530cc739fad7e9b326e53a386cef3
omkarjagtap1011 <126784997+omkarjagtap1011@users.noreply.github.com>
2026-05-23 22:51:27 +0530

    Time: 2 ms (32.68%), Space: 11.3 MB (18.56%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> res;
+        for(int i=0; i<=n; i++){
+            int num = i;
+            int count = 0;
+            while(num != 0){
+                int b = num & 1;
+                if(b) count++;
+                num>>=1;
+            }
+            res.push_back(count);
+        }
+        return res;
+    }
+};
