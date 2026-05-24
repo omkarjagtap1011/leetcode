6818d9882a6d7e2fa2a61f4721b02a5b6c6d467c
omkarjagtap1011 <126784997+omkarjagtap1011@users.noreply.github.com>
2026-05-24 17:21:29 +0530

    Time: 0 ms (100%), Space: 35.8 MB (57.56%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,28 +1,25 @@
 class Solution {
-    bool f(int idx, vector<int>& arr, int n, vector<bool>& visited, vector<int>& dp){
-        if(arr[idx] == 0) return true;
-        if(visited[idx]) return false;
-        if(dp[idx] !=- 1) return dp[idx];
-        visited[idx] = true;
-
-        bool fs = false;
-        bool ss = false;
-        if(idx + arr[idx] <n){
-            fs = f(idx+arr[idx], arr, n, visited, dp);
-        }
-
-        if(idx-arr[idx] >= 0){
-            ss = f(idx - arr[idx], arr, n, visited, dp);
-        }
-
-        visited[idx] = false;
-        return dp[idx] = (fs || ss);
-    }    
 public:
     bool canReach(vector<int>& arr, int start) {
         int n = arr.size();
         vector<bool> visited(n, false);
-        vector<int> dp(n, -1);
-        return f(start, arr, n, visited, dp);
+        queue<int> q;
+
+        q.push(start);
+
+        while(!q.empty()){
+            int i = q.front();
+            q.pop();
+
+            if(i<0 || i>=n || visited[i]) continue;
+
+            visited[i] = true;
+
+            if(arr[i] == 0) return true;
+
+            q.push(i + arr[i]);
+            q.push(i - arr[i]);
+        }
+        return false;
     }
 };
