7637055a6df84cc0e67bb439c84bf3b4ceafd45c
omkarjagtap1011 <126784997+omkarjagtap1011@users.noreply.github.com>
2026-05-24 16:59:36 +0530

    Time: 3 ms (45.37%), Space: 43.4 MB (5.74%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,27 +1,28 @@
 class Solution {
-    bool f(int idx, vector<int>& arr, int n, vector<bool>& visited){
+    bool f(int idx, vector<int>& arr, int n, vector<bool>& visited, vector<int>& dp){
         if(arr[idx] == 0) return true;
         if(visited[idx]) return false;
-
+        if(dp[idx] !=- 1) return dp[idx];
         visited[idx] = true;
 
         bool fs = false;
         bool ss = false;
         if(idx + arr[idx] <n){
-            fs = f(idx+arr[idx], arr, n, visited);
+            fs = f(idx+arr[idx], arr, n, visited, dp);
         }
 
         if(idx-arr[idx] >= 0){
-            ss = f(idx - arr[idx], arr, n, visited);
+            ss = f(idx - arr[idx], arr, n, visited, dp);
         }
 
         visited[idx] = false;
-        return (fs || ss);
+        return dp[idx] = (fs || ss);
     }    
 public:
     bool canReach(vector<int>& arr, int start) {
         int n = arr.size();
         vector<bool> visited(n, false);
-        return f(start, arr, n, visited);
+        vector<int> dp(n, -1);
+        return f(start, arr, n, visited, dp);
     }
 };
