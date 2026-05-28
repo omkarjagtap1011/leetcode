b6842344ce83759e582abf3c1e6b14495b7a26de
omkarjagtap1011 <126784997+omkarjagtap1011@users.noreply.github.com>
2026-05-29 02:41:33 +0530

    Time: 15 ms (5.43%), Space: 15.2 MB (20.62%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,28 +1,4 @@
 class Solution {
-    int recur(int row, int last, vector<vector<int>>& matrix, int n, vector<vector<int>>& dp){
-        if(dp[row][last] != -1) return dp[row][last];
-        if(row==0){
-            int mini = INT_MAX;
-            for(int i=0; i<n; i++){
-                if(abs(last-i) <= 1){
-                    mini = min(mini, matrix[0][i]);
-                }
-            }
-
-            return dp[row][last] = mini;
-        }
-
-        int mini = INT_MAX;
-
-        for(int i=0; i<n; i++){
-            if(row==n-1 || abs(last-i) <= 1){
-                int score = matrix[row][i] + recur(row-1, i, matrix, n, dp);
-                mini = min(mini, score);
-            }
-        }
-
-        return dp[row][last] = mini;
-    }
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
