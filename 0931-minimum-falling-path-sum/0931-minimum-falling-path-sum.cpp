18c8f099d454f44a94dc7f15340ed6084fe8aae7
omkarjagtap1011 <126784997+omkarjagtap1011@users.noreply.github.com>
2026-05-31 04:18:04 +0530

    Time: 0 ms (100%), Space: 14.3 MB (95.94%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -2,11 +2,6 @@ class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> dp(n, vector<int>(n));
-
-        for(int i=0; i<n; i++){
-            dp[n-1][i] = matrix[n-1][i];
-        }
 
         for(int i=n-2; i>=0; i--){
             for(int j=0; j<n; j++){
@@ -14,17 +9,17 @@ public:
                 int rightdown = 100000;
 
                 if(j-1>=0)
-                    leftdown = matrix[i][j] + dp[i+1][j-1];
+                    leftdown = matrix[i][j] + matrix[i+1][j-1];
 
-                int down = matrix[i][j] + dp[i+1][j];
+                int down = matrix[i][j] + matrix[i+1][j];
 
                 if(j+1<n)
-                    rightdown = matrix[i][j] + dp[i+1][j+1];
+                    rightdown = matrix[i][j] + matrix[i+1][j+1];
 
-                dp[i][j] = min({leftdown, down, rightdown});
+                matrix[i][j] = min({leftdown, down, rightdown});
             }
         }
 
-        return *min_element(dp[0].begin(), dp[0].end());
+        return *min_element(matrix[0].begin(), matrix[0].end());
     }
 };
