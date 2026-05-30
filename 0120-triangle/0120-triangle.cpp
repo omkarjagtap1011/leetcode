463fcb83b4280c738ec9993a366d9f26ec688d0c
omkarjagtap1011 <126784997+omkarjagtap1011@users.noreply.github.com>
2026-05-31 03:34:54 +0530

    Time: 0 ms (100%), Space: 13.3 MB (52.27%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,45 +1,20 @@
 class Solution {
-    int recur(int row, int last, vector<vector<int>>& triangle, vector<vector<int>>& dp){
-        if(row==0){
-            return triangle[0][0];
-        }
-        if(dp[row][last] != -1){
-            return dp[row][last];
-        }
-        int mini = INT_MAX;
-        int len = triangle[row].size();
-        
-        if(last<len){
-            mini = min(mini, triangle[row][last] + recur(row-1, last, triangle, dp));
-        }
-
-        if(last-1 >=0){
-            mini = min(mini, triangle[row][last-1] + recur(row-1, last-1, triangle, dp));
-        }
-
-        return dp[row][last] = mini;
-    }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int m = triangle.size();
-        int n = triangle[m-1].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        dp[0][0] = triangle[0][0];
+        vector<vector<int>> dp(m, vector<int>(m, -1));
+        for(int i=0; i<m; i++){
+            dp[m-1][i] = triangle[m-1][i];
+        }
 
-        for(int row=1; row<m; row++){
-            int len = triangle[row].size();
-            
-
-            for(int j=0; j<len; j++){
-                int mini = INT_MAX;
-
-                if(j<len-1) mini = min(mini, dp[row-1][j]);
-
-                if(j-1>=0) mini = min(mini, dp[row-1][j-1]);
-
-                dp[row][j] = triangle[row][j] + mini;
+        for(int i=m-2; i>=0; i--){
+            int n = triangle[i].size();
+            for(int j=0; j<n; j++){
+                int down = triangle[i][j] + dp[i+1][j];
+                int diag = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(down, diag);
             }
         }
-        return *min_element(dp[m-1].begin(), dp[m-1].end());
+        return dp[0][0];
     }
 };
