c93567479c61fe77107b9934604c4f78f8ca32ac
omkarjagtap1011 <126784997+omkarjagtap1011@users.noreply.github.com>
2026-05-31 12:41:33 +0530

    Time: 27 ms (84.23%), Space: 140.6 MB (5.11%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.cpp
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -3,11 +3,16 @@ public:
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
         int n = asteroids.size();
         long long temp = mass;
-        sort(asteroids.begin(), asteroids.end());
-
+        vector<int> count(100001, 0);
         for(int i=0; i<n; i++){
-            if(asteroids[i] > temp) return false;
-            temp+=asteroids[i];
+            count[asteroids[i]]++;
+        }
+
+        for(int i=1; i<100001; i++){
+            if(count[i] != 0){
+                if(i>temp) return false;
+                temp += (1LL * i * count[i]);
+            }
         }
         return true;
     }
