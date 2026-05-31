fdfa114e13ee5f57256d0061c33d8052cd8ad99d
omkarjagtap1011 <126784997+omkarjagtap1011@users.noreply.github.com>
2026-05-31 15:00:21 +0530

    Time: 50 ms (53.46%), Space: 21.1 MB (27.74%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,24 +1,4 @@
 class Solution {
-    int recur(int row, int col1, int col2, vector<vector<int>>& grid, int m, int n, vector<vector<vector<int>>>& dp){
-        if(col1<0 || col1>=n || col2<0 || col2>=n) return -1e8;
-
-        if(row==m-1){
-            if(col1==col2) return grid[row][col1];
-            else return grid[row][col1] +  grid[row][col2];
-        }
-
-        if(dp[row][col1][col2] != -1) return dp[row][col1][col2];
-
-        int maxi = -1e8;
-        for(int dj1 = -1; dj1<2; dj1++){
-            for(int dj2 = -1; dj2<2; dj2++){
-                maxi = max(maxi, recur(row+1, col1+dj1, col2+dj2, grid, m, n, dp));
-            }
-        }
-
-        if(col1==col2) return dp[row][col1][col2] = grid[row][col1] + maxi;
-        else return dp[row][col1][col2] = grid[row][col1] + grid[row][col2] + maxi;
-    }
 public:
     int cherryPickup(vector<vector<int>>& grid) {
         int m = grid.size();
