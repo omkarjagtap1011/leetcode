7a9a1581277155e27c11646259ebef71b4672fbe
omkarjagtap1011 <126784997+omkarjagtap1011@users.noreply.github.com>
2026-05-31 11:31:48 +0530

    Time: 0 ms (100%), Space: 19.8 MB (95.76%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -4,14 +4,21 @@ public:
         int n = matrix.size();
 
         for(int i=n-2; i>=0; i--){
+            int min1 = INT_MAX;
+            int min2 = INT_MAX;
+            int minCol = -1;
             for(int j=0; j<n; j++){
-                int mini = INT_MAX;
-                for(int prev=0; prev<n; prev++){
-                    if(prev!=j){
-                        mini = min(mini, matrix[i+1][prev]);
-                    }
+                if(matrix[i+1][j] < min1){
+                    min2 = min1;
+                    min1 = matrix[i+1][j];
+                    minCol = j;
+                }else if(matrix[i+1][j] < min2){
+                    min2 = matrix[i+1][j];
                 }
-                matrix[i][j] += mini;
+            }
+
+            for(int j=0; j<n; j++){
+                matrix[i][j] += (j==minCol?min2:min1);
             }
         }
 
